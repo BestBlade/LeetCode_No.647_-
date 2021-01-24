0c20af5d0aff580b09183ca3d6de5e1ba72394b3
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-24 11:07:43 +0800

    Add files via upload

---
+++ leetcode_no647_最长连续递增序列_w1.cpp
@@ -0,0 +1,92 @@
+﻿/* ------------------------------------------------------------------|
+给定一个未经排序的整数数组，找到最长且 连续递增的子序列，并返回该序列的长度。
+
+连续递增的子序列 可以由两个下标 l 和 r（l < r）确定，如果对于每个 l <= i < r，都有 nums[i] < nums[i + 1] ，那么子序列 [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] 就是连续递增子序列。
+
+ 
+
+示例 1：
+
+输入：nums = [1,3,5,4,7]
+输出：3
+解释：最长连续递增序列是 [1,3,5], 长度为3。
+尽管 [1,3,5,7] 也是升序的子序列, 但它不是连续的，因为 5 和 7 在原数组里被 4 隔开。
+示例 2：
+
+输入：nums = [2,2,2,2,2]
+输出：1
+解释：最长连续递增序列是 [2], 长度为1。
+ 
+
+提示：
+
+0 <= nums.length <= 104
+-109 <= nums[i] <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	贪心法
+*
+*	执行用时：4 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：10.7 MB, 在所有 C++ 提交中击败了96.62%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+int findLengthOfLCIS(vector<int>& nums) {
+    if (nums.size() < 2) {
+        return nums.size();
+    }
+    int l = 0;
+    int r = 1;
+    int res = 1;
+    while (r < nums.size()) {
+        if (nums[r] <= nums[r - 1]) {
+            res = max(res, r - l);
+            l = r;
+        }
+        r++;
+    }
+    return max(res, r - l);
+}
